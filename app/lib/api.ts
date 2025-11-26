4535183a037e87be36a7375936ad286f7940813b
marcioalvs <marcioalvs@gmail.com>
2025-11-26 15:33:32 -0300

    .

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,4 +1,4 @@
-const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://strapi:1337';
 
 export async function fetchPosts() {
   const res = await fetch(`${STRAPI_URL}/api/posts?populate=*`);
